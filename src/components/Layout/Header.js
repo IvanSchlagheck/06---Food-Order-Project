f2312669735217218f2c5bab5a19dcc015c84337
ivan schlagheck <ivansch05@gmail.com>
2022-01-20 10:33:13 +0100

    added Header Module, including styling

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+import 
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </Fragment>
+  )
+};
+
+export default Header
