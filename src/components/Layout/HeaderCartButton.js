6eba96d8310bdd82049a43949b3d89b1990a8572
ivan schlagheck <ivansch05@gmail.com>
2022-01-23 12:13:17 +0100

    finished animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 // React Import
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 // Styles Import
 import styles from "./HeaderCartButton.module.css";
 // Components Import
@@ -7,18 +7,30 @@ import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../Store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
-    return currentNumber + item.amount
-  },
-  0);
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
 
-  const btnClasses = `${styles.button} ${styles.bump}`
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`; // 2- if btnIdHighlighted is true, add the bump class otherwise not
 
   useEffect(() => {
-    
-  }, [])
+    if (items.length === 0) {
+      return;
+    } else setBtnIsHighlighted(true); // 1 - i change btnIsHighlighted to true
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
     <button className={btnClasses} onClick={props.onCartClick}>
